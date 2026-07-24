989984912bab0d26730d96d72a2b38fa40464a0b
srikanthchalla <srikanthchalla@gmail.com>
2026-07-24 21:24:53 +0530

    added

--- 00-vpc/parameterssm.tf
+++ 00-vpc/parameterssm.tf
@@ -4,3 +4,25 @@ resource "aws_ssm_parameter" "vpc_id" {
     value = module.vpc.vpc_id
   
 }
+
+resource "aws_ssm_parameter" "public_subnet_ids" {
+    name = "/${var.project}/${var.environment}/public_subnet_ids"
+    type = "String"
+    value = join(",", module.vpc.public_subnet_ids)
+  
+}
+
+
+resource "aws_ssm_parameter" "private_subnet_ids" {
+    name = "/${var.project}/${var.environment}/private_subnet_ids"
+    type = "String"
+    value = join(",", module.vpc.private_subnet_ids)
+  
+}
+
+resource "aws_ssm_parameter" "database_subnet_ids" {
+    name = "/${var.project}/${var.environment}/database_subnet_ids"
+    type = "String"
+    value = join(",", module.vpc.database_subnet_ids)
+  
+}
