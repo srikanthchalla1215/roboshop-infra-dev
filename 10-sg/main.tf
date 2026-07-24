92289e279ccabcb91cdd5cd5170d6480d9338ec9
srikanthchalla <srikanthchalla@gmail.com>
2026-07-24 23:07:06 +0530

    added sg_rules

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -3,8 +3,7 @@ module "sg" {
     source = "git::https://github.com/srikanthchalla1215/terraform-aws-sg.git?ref=main"
     project = var.project
     environment = var.environment
-    sg_name = var.sg_names[count.index]
+    sg_name = replace(var.sg_names[count.index], "_", "-")
     vpc_id = local.vpc_id
 
-  
 }
