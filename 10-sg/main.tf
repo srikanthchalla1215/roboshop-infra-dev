712d7dd8ba18ed750e50500a31733c0cef2008c0
srikanthchalla <srikanthchalla@gmail.com>
2026-07-24 22:19:15 +0530

    added security group

---
+++ 10-sg/main.tf
@@ -0,0 +1,10 @@
+module "sg" {
+    count = length(var.sg_names)
+    source = "git::https://github.com/srikanthchalla1215/terraform-aws-sg.git?ref=main"
+    project = var.project
+    environment = var.environment
+    sg_name = var.sg_names[count.index]
+    vpc_id = local.vpc_id
+
+  
+}
