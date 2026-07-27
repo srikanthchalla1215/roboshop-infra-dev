ae35290fb5cccbee237f038521c628a2918eb6fa
srikanthchalla <srikanthchalla@gmail.com>
2026-07-27 23:30:43 +0530

    added

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -38,3 +38,45 @@ resource "terraform_data" "mongodb" {
     }
   
 }
+
+
+resource "aws_instance" "redis" {
+    instance_type = "t3.micro"
+    ami = local.ami_id
+    vpc_security_group_ids = [local.mongodb_sg_id]
+    subnet_id = local.database_subnet_id
+
+    tags = merge(
+        {
+            Name ="${var.project}-${var.environment}-redis"
+        },
+        local.common_tags
+    )
+}
+
+resource "terraform_data" "redis" {
+    triggers_replace = [
+        aws_instance.redis.id
+    ]
+
+    connection {
+        type = "ssh"
+        user = "ec2-user"
+        password = "DevOps321"
+        host = aws_instance.redis.private_ip
+  
+    }
+
+    provisioner "file" {
+        source = "bootstrap.sh"
+        destination = "/tmp/bootstrap.sh"  
+      
+    }
+    provisioner "remote-exec" {
+        inline = [ 
+            "chmod +x /tmp/bootstrap.sh",
+            "sudo sh /tmp/bootstrap.sh redis ${var.environment}"
+         ]
+      
+    }
+}
