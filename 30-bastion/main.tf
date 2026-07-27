f3578b2a173acca8e7da9daaf05a7033878d44df
srikanthchalla <srikanthchalla@gmail.com>
2026-07-27 21:27:35 +0530

    added bastion code

---
+++ 30-bastion/main.tf
@@ -0,0 +1,58 @@
+resource "aws_instance" "bastion" {
+    instance_type = "t3.micro"
+    ami = local.ami_id
+    vpc_security_group_ids = [ local.bastion_sg_id ]
+    subnet_id = local.public_subnet_id
+    iam_instance_profile = aws_iam_instance_profile.bastion.name
+    user_data = file("bastion.sh")
+
+    root_block_device {
+      volume_size = 50
+      volume_type = "gp3"
+    }
+    tags = merge ( {
+        Name = "Roboshopbation"
+    },
+    local.common_tags
+        
+    )
+}
+
+resource "aws_iam_role" "bastion" {
+  name = "RoboShopDevBastion"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = merge(
+    {
+        Name = "RoboShopDevBastion"
+    },
+    local.common_tags
+  )
+}
+
+
+resource "aws_iam_role_policy_attachment" "bastion" {
+    role = aws_iam_role.bastion.name
+    policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  
+}
+
+resource "aws_iam_instance_profile" "bastion" {
+  name = "${var.project}-${var.environment}-bastion"
+  role = aws_iam_role.bastion.name
+}
